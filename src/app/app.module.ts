c84880131662c8a51b114713fd0b9da8be3803fa
Kimberly Aguilar Hermoso <kimaguilar2017@gmail.com>
2022-06-24 19:19:58 -0700

    comments added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AppservService } from './appserv.service';
@@ -11,9 +10,10 @@ import { AppservService } from './appserv.service';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [AppservService],  //Injecting dependencies which is servername created from commandterminal($ng g server appserv)
   bootstrap: [AppComponent]
+
 })
 export class AppModule { }
